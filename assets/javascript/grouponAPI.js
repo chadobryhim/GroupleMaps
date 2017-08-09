38d4035c223af9682b6dc60d73ebe47b74221dab
Jay Sullivan <jaysull514@gmail.com>
2017-08-08 19:49:05 -0500

    Refined api call url building

--- assets/javascript/grouponAPI.js
+++ assets/javascript/grouponAPI.js
@@ -3,15 +3,20 @@ $(document).on('click', '#user-input-button', function() {
     var location = $('#user-location').val();
     console.log(location);
 
+    var testLocation = {
+        "location": {
+            "lat": 51.0,
+            "lng": -0.1
+        },
+        "accuracy": 1200.4
+    };
+
+    console.log(testLocation.location.lat);
+
     var dealCategory = $('#deal-category').val();
     console.log(dealCategory);
 
-    var searchAttributeThree = $('searchAttributeThreeIDorClassHere').val();
-    var searchAttributeFour = $('searchAttributeFourIDorClassHere').val();
-    var searchAttributeFive = $('searchAttributeFiveIDorClassHere').val();
-    var searchAttributeSix = $('searchAttributeSixIDorClassHere').val();
-
-    var xhr = $.get('https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0&filters=category:' + dealCategory);
+    var xhr = $.get('https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0&filters=category:' + dealCategory + '&lat=' + testLocation.location.lat + '&long=' + testLocation.location.lng);
 
     xhr.done(function(result) {
         console.log(result);
