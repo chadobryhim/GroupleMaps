6bc1e7194a26c5e58aaef51a216a11c9b3018dc5
Jay Sullivan <jaysull514@gmail.com>
2017-08-08 19:36:09 -0500

    Added id's for existing search fields and incorporated on the click event for the go button

---
+++ assets/javascript/grouponAPI.js
@@ -0,0 +1,20 @@
+$(document).on('click', '#user-input-button', function() {
+
+    var location = $('#user-location').val();
+    console.log(location);
+
+    var dealCategory = $('#deal-category').val();
+    console.log(dealCategory);
+
+    var searchAttributeTwo = $('searchAttributeTwoIDorClassHere').val();
+    var searchAttributeThree = $('searchAttributeThreeIDorClassHere').val();
+    var searchAttributeFour = $('searchAttributeFourIDorClassHere').val();
+    var searchAttributeFive = $('searchAttributeFiveIDorClassHere').val();
+    var searchAttributeSix = $('searchAttributeSixIDorClassHere').val();
+
+    var xhr = $.get('https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0&filters=category:' + dealCategory);
+
+    xhr.done(function(result) {
+        console.log(result);
+    });
+});
