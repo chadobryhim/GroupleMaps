60a968a07b5dbec6c8765064df39d6fd7f0c165d
Jay Sullivan <jaysull514@gmail.com>
2017-08-10 21:09:27 -0500

    Updated the object creator to include each individual deal per location

--- assets/javascript/grouponAPI.js
+++ assets/javascript/grouponAPI.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-$(document).on('click', '#user-input-button', function() {
-
-   var location = $('#user-location').val();
-   console.log(location);
-
-    var testLocation = {
-        "location": {
-            "lat": 51.0,
-            "lng": -0.1
-        },
-        "accuracy": 1200.4
-   };
-
-    console.log(testLocation.location.lat);
-
-    var dealCategory = $('#deal-category').val();
-    console.log(dealCategory);
-
-    var xhr = $.get('https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0&filters=category:' + dealCategory + '&lat=' + testLocation.location.lat + '&long=' + testLocation.location.lng);
-
-    xhr.done(function(result) {
-        console.log(result);
-    });
-});
-=======
 var dealReturn = [];
 
 $(document).on('click', '#user-input-button', function() {
@@ -104,4 +78,3 @@ function userInputEntered() {
         }
     });
 };
->>>>>>> 42688dbd3d10ff8df34f01f982db5f268f6eb0d1
