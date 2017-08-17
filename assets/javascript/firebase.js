b23f59d9aa40ecd77a6aa5a1c262dfb7c3e3d719
teenagegrandma <rebecca.preston90@gmail.com>
2017-08-17 18:34:21 -0500

    html and firebase changes

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,4 +1,5 @@
-  // Initialize Firebase
+
+// Initialize Firebase
 var config = {
     apiKey: "AIzaSyBqIeOVvAdTRF0NG33glVdOwfqQYcfaIZs",
     authDomain: "firegroupmap.firebaseapp.com",
@@ -15,41 +16,49 @@ var database = firebase.database();
 $("#add-user-btn").on("click", function(event) {
   event.preventDefault();
 
+  debugger;
+
   //Grabs user input
-  var userName = $("#user-name-input").val().trim();
   var userEmail = $("#user-email-input").val().trim();
-  //var userPassword = $("#user-password-input").val().trim();
-  var userFavorites = $("#user-favorites-input").val().trim();
-  var userLocation = $("#user-location-input").val().trim();
+  var userPassword = $("#user-password-input").val().trim();
 
   //temporarily holds data for userLocation
   var newUser = {
-    name: userName,
     email: userEmail,
-    //password: userPassword,
-    favorites: userFavorites,
-    location: userLocation
+    password: userPassword,
   };
 
   //upload data to the database
   database.ref().push(newUser);
 
-  //console longitude
-  console.log(newUser.name);
-  console.log(newEmail.email);
-  //console.log(newPassword.password);
-  console.log(newFavorite.favorites);
-  console.log(newLocation.location);
+  console.log(newUser.email);
+  console.log(newUser.password);
 
   //alert
   alert("Welcome to GrouponMapper!");
 
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
+  //Handle errors
+  var errorCode = error.code;
+  var errorMessage = error.message;
+
+});
+
   //clears input-boxes
-  $("#user-name-input").val("");
   $("#user-email-input").val("");
-  //$("#user-password-input").val("");
-  $("#user-favorites-input").val("");
-  $("#user-location-input").val("");
+  $("#user-password-input").val("");
+});
+
+$("#add-sign-btn").on("click", function(event) {
+  event.preventDefault();
+
+
+firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
+  //Handle errors
+  var errorCode = error.code;
+  var errorMessage = error.message;
+})
+
 });
 
 //create event for adding employees to database
@@ -58,28 +67,17 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
   console.log(childSnapshot.val());
 
   //store it!
-  var userName = childSnapshot.val().name;
   var userEmail = childSnapshot.val().email;
-  //var userPassword = childSnapshot.val().password;
-  var userFavorites = childSnapshot.val().favorites;
-  var userLocation = childSnapshot.val().location;
+  var userPassword = childSnapshot.val().password;
 
   //console log
-  console.log(userName);
   console.log(userEmail);
-  //console.log(userPassword);
-  console.log(userFavorites);
-  console.log(userLocation);
-}
-
-SimpleLogin authClient = new SimpleLogin(myRef, getApplicationContex());
-authClient.loginWithEmail("email@example.com", "very secret", new SimpleLoginAuthenticatedHandler() {
-  public void authenticated(FirebaseSimpleLoginError error, FirebaseSimpleLoginUser user) {
-    if(error != null) {
-      // There was an error logging into this account
-    }
-    else {
-      // We are now logged in
-    }
-  }
+  console.log(userPassword);
 });
+
+firebase.auth().onAuthStateChanged(function (user) {
+  debugger;
+  console.log(user);
+});
+
+//
